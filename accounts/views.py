196d15c16caa99bdd081d1a632ea3d89ee9e29c1
Keith Erskine <kerskine@gmail.com>
2018-07-07 18:13:25 +0000

    logout with message created

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+    
+def logout(request):
+    """Logout the User"""
+    auth.logout(request)
+    messages.success(request, 'You have been successfully logged out')
+    return redirect(reverse('index'))
+    
